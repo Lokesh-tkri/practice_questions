8f8ca73071055faa33e95717b3a662ea6062c681
Lokesh <lokesh.t43@live.com>
2024-04-29 23:28:27 +0530

    Create problem13.java

---
+++ js/problem13.java
@@ -0,0 +1,28 @@
+// Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the N-Star Triangle.
+// Example:
+
+// Input: ‘N’ = 3
+
+// Output: 
+
+//   *
+//  ***
+// *****
+
+public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+        for(int i=0;i<n;i++){
+            //number of spaces = i-1 
+            //number of stars = 2*j + 1
+            for(int k=n; k>i+1;k--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<(2*i + 1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
