f45af309d51bef7c545409c3b0e34798b06d0271
Lokesh <lokesh.t43@live.com>
2024-05-15 09:38:19 +0530

    Create problem18.java

---
+++ striver cheat sheet problems/problem18.java
@@ -0,0 +1,25 @@
+// Aryan and his friends are very fond of patterns. For a given integer ‘N’, they want to make the Increasing Number Triangle.
+// Example:
+
+// Input: ‘N’ = 3
+
+// Output: 
+
+// 1
+// 2 3
+// 4 5 6
+
+public class Solution {
+    public static void nNumberTriangle(int n) {
+        // Write your code here
+        int start = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<i; j++){
+                System.out.print(start);
+                System.out.print(" ");
+                start++;
+            }
+            System.out.println();
+        }
+    }
+}
